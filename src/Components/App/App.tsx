a1a06cbd61005a691dec53770f24dca603a66fc1
Ehab Abu Hadwan <ehabopto@gmail.com>
2025-04-23 15:53:33 +0300

    initial version & basic style

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+      <label>
+        כתובת
+      </label>
+      <input type="text" />
+      <hr />
+      <button>
+        הצג את הקואורדינטות
+      </button>
+      </div>
+    </>
+  )
+}
+
+export default App
